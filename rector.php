b1ad9af784ff62c5933af8fe5332d038ca092f6e
Saša Stamenković <umpirsky@gmail.com>
2022-02-01 09:01:02 +0100

    Initial commit

---
+++ rector.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/ProblematicTest.php'
+    ]);
+
+    $services = $containerConfigurator->services();
+    $services->set(Rector\PHPUnit\Rector\ClassMethod\MigrateAtToConsecutiveExpectationsRector::class);
+};
